25c79866a00ab66893a1a037ae63371140464094
Dhruv-Nayar <nayardhruv0@gmail.com>
2015-02-19 17:07:27 -0500

    only writes to console on song change

--- background.js
+++ background.js
@@ -1,4 +1,6 @@
 
+currentSong = "";
+
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
     switch(request.type) {
         case "get-song":
@@ -12,8 +14,16 @@ var getSong = function() {
 	chrome.tabs.query({url: "*://play.spotify.com/*"}, function(results) {
 		//chrome.browserAction.setBadgeText({text: (results.length).toString()});
 		for (var i = 0; i < results.length; i++) {
-			chrome.extension.getBackgroundPage().console.log(results[i].title);
+			//chrome.extension.getBackgroundPage().console.log(results[i].title);
+			songChange(resutls[i].title);
 		}
 	});
 	t = setTimeout(function() { getSong() }, 1000);
+}
+
+var songChange = function(song){
+	if(song != currentSong){
+		currentSong = song;
+		chrome.extension.getBackgroundPage().console.log(song);
+	}
 }
