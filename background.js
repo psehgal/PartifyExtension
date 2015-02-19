cd2a11f5c87a1be071eabf07205fe8c939ebe216
Parth Sehgal <parth.sehgal@gatech.edu>
2015-02-19 17:00:52 -0500

    cleaned up some code

--- background.js
+++ background.js
@@ -8,17 +8,6 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
     return true;
 });
 
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {          
-	// alert(changeInfo.status);
-	// if (changeInfo.status == 'complete') {
-	// 	if (tab.url.indexOf("in.yahoo.mail") !== -1) {
-	// 	alert(tab.url);
-	// 	chrome.tabs.update(tabId, { url: "https://accounts.google.com/ServiceLogin" });
-	// 	//injectToTab(tab);
-	// 	}
-	// }
-});
-
 var getSong = function() {
 	chrome.tabs.query({url: "*://play.spotify.com/*"}, function(results) {
 		//chrome.browserAction.setBadgeText({text: (results.length).toString()});
--- popup.js
+++ popup.js
@@ -1,31 +1,7 @@
-var t;
-
 window.onload = function() {
-    // document.getElementById("button").onclick = function {
-    //     chrome.extension.sendMessage({
-    //         type: "get-song"
-    //     });
-    // }
     document.getElementById("button").onclick = function() {
     	chrome.extension.sendMessage({
     		type: "get-song"
     	})
     }
-}
-
-// while(1) {
-// 	setTimeout(function() {
-// 		chrome.extension.sendMessage({
-// 			type: "get-song"
-// 		});
-// 	}, 1000);
-// }
-
-
-
-function timedMessage() {
-    chrome.runtime.sendMessage({
-    	type: "get-song"
-    })
-    t = setTimeout(function() { timedMessage(); }, 2000);
 }
