163b83a63d573bebc10552b317e1b4f9c32a2d6f
Parth Sehgal <parth.sehgal@gatech.edu>
2015-02-27 21:37:19 -0500

    creates playlist

--- background.js
+++ background.js
@@ -1,14 +1,32 @@
 
 currentSong = "";
+playlistId = "";
 
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
     switch(request.type) {
         case "get-song":
             getSong();
+        case "login":
+        	createPlaylistWithLogin();
+        	sendResponse({ msg : playlistId });
+        case "create-playlist":
+        	createPlaylist();
         break;
     }
     return true;
 });
+
+var createPlaylistWithLogin = function() {
+	playlistId = makeid();
+	var url = "http://localhost:8081/login/";
+	var http = new XMLHttpRequest();
+	var fullUrl = url + playlistId;
+	chrome.tabs.create( {url : fullUrl} );
+}
+
+var login = function() {
+	chrome.tabs.create({ url : "http://partify.herokuapp.com/login"});
+}
 
 var getSong = function() {
 	chrome.tabs.query({url: "*://play.spotify.com/*"}, function(results) {
@@ -29,9 +47,18 @@ var songChange = function(song){
 	}
 }
 
-
 //SHIV this is where the post would happen
 //The song parameter is the name of the currently playing song in the playlist
 var songChangeAction = function(song){
 	chrome.extension.getBackgroundPage().console.log(song);
+}
+
+function makeid() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 5; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
 }
--- popup.js
+++ popup.js
@@ -1,7 +1,37 @@
 window.onload = function() {
-    document.getElementById("button").onclick = function() {
+    document.getElementById("song_button").onclick = function() {
     	chrome.extension.sendMessage({
     		type: "get-song"
+    	});
+    }
+    document.getElementById("login_button").onclick = function() {
+    	chrome.extension.sendMessage({
+    		type: "login"
+    	}, function(response) {
+    		renderAccessToken(response.msg);
+    	});
+		renderAccessToken("access token");
+    }
+    document.getElementById("playlist_button").onclick = function() {
+    	chrome.extension.sendMessage({
+    		type: "create-playlist"
     	})
     }
 }
+
+function renderAccessToken(stringAccessToken) {
+	document.getElementById("access_token").textContent = stringAccessToken;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var links = document.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        (function () {
+            var ln = links[i];
+            var location = ln.href;
+            ln.onclick = function () {
+                chrome.tabs.create({active: true, url: location});
+            };
+        })();
+    }
+});
